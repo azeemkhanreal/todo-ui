f51e9aec27b8e5ecd1c152ed5e6f413033bc0c58
Azeem Khan <azeemkhanreal@gmail.com>
2022-02-26 23:50:06 +0530

    delete task functionality added

--- src/App.js
+++ src/App.js
@@ -1,33 +1,45 @@
 import React, { useState } from "react";
 
 const App = () => {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState();
   const [task, setTask] = useState([]);
 
-  const handleInput = (e) => {
-    setInput(e.target.value);
+  const handleInput = (value) => {
+    setInput(value);
   };
-  const saveTask = () => {
+  const addTask = (e) => {
+    e.preventDefault();
     setTask([...task, input]);
     setInput("");
   };
+
+  const deleteTask = (key) => {
+    let tasks = task.filter((item, index) => index !== key);
+    setTask([...tasks]);
+  };
+
   console.log(task);
   return (
     <div className="App">
-      <h1>Write Task </h1>
+      <h1>Write Task</h1>
       <div className="todo_form">
-        <form>
-          <input type="text" value={input} onChange={(e) => handleInput(e)} />
+        <form onSubmit={addTask}>
+          <input
+            type="text"
+            value={input}
+            onChange={(event) => handleInput(event.target.value)}
+          />
+          <button onClick={addTask}>Add Task</button>
         </form>
       </div>
+
       <div className="todo_list">
         {task.map((item, index) => (
-          <p>{item}</p>
+          <>
+            <p>{item}</p>
+            <button onClick={() => deleteTask(index)}>Delete Task</button>
+          </>
         ))}
-      </div>
-      <div className="todoButtons">
-        <button onClick={saveTask}>Add Task</button>
-        <button>Delete Task</button>
       </div>
     </div>
   );
