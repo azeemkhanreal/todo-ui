de586e01a4ae79c3ffe6ad6e2c7d7d58e1c14d69
Azeem Khan <azeemkhanreal@gmail.com>
2022-03-02 22:07:02 +0530

    styling | add button | empty input add task bug fixed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
+import ListTasks from "./components/ListTasks";
+import "./App.css";
 
 const App = () => {
-  const [input, setInput] = useState();
   const [task, setTask] = useState([]);
-
-  const handleInput = (value) => {
-    setInput(value);
-  };
+  const [input, setInput] = useState();
+  // when user enter add task button then addTask function take input and set into input state
   const addTask = (e) => {
     e.preventDefault();
-    setTask([...task, input]);
-    setInput("");
+    if (input) {
+      setTask([...task, input]);
+      setInput("");
+    }
+  };
+  // handle input value and set value into input state
+  const handleInput = (value) => {
+    setInput(value);
   };
 
   const deleteTask = (key) => {
@@ -18,28 +23,23 @@ const App = () => {
     setTask([...tasks]);
   };
 
-  console.log(task);
   return (
     <div className="App">
-      <h1>Write Task</h1>
-      <div className="todo_form">
-        <form onSubmit={addTask}>
-          <input
-            type="text"
-            value={input}
-            onChange={(event) => handleInput(event.target.value)}
-          />
-          <button onClick={addTask}>Add Task</button>
-        </form>
-      </div>
-
-      <div className="todo_list">
-        {task.map((item, index) => (
-          <>
-            <p>{item}</p>
-            <button onClick={() => deleteTask(index)}>Delete Task</button>
-          </>
-        ))}
+      <div className="todo-container">
+        <h1 className="todo-title">Todo's Task</h1>
+        <div className="todo-form">
+          <form onSubmit={addTask}>
+            <input
+              type="text"
+              value={input}
+              onChange={(event) => handleInput(event.target.value)}
+            />
+            <button onClick={addTask}>Add Task</button>
+          </form>
+        </div>
+        <div className="todo-list">
+          <ListTasks data={task} deleteFunc={deleteTask} />
+        </div>
       </div>
     </div>
   );
